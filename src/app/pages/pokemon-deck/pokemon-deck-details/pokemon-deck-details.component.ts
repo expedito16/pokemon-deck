5c96c4c76176d0b4fff15635b067160da96f1efd
Expedito <expeditopaivaneto@gmail.com>
2024-03-12 12:45:18 -0300

    Criando layout, estilizacao e integracao do componente de visualizar baralho pokemon

--- src/app/pages/pokemon-deck/pokemon-deck-details/pokemon-deck-details.component.ts
+++ src/app/pages/pokemon-deck/pokemon-deck-details/pokemon-deck-details.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PokemonDeckService } from '../services/pokemon-deck.service';
 
 @Component({
   selector: 'app-pokemon-deck-details',
   templateUrl: './pokemon-deck-details.component.html',
   styleUrls: ['./pokemon-deck-details.component.scss']
 })
-export class PokemonDeckDetailsComponent {
+export class PokemonDeckDetailsComponent implements OnInit {
+  id: number = 0
+  deckName: any[] = [];
+  dataDeck: any[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: PokemonDeckService
+  ) { }
+
+  ngOnInit(): void {
+    this.viewDeckInformation();
+  }
+
+  viewDeckInformation() {
+    this.id = this.route.snapshot.params['id'];
+    console.log("id: ", this.id);
+    this.service.getDecksPokemonById(this.id).subscribe((data: any) => {
+      this.deckName = data.deckName;
+      this.dataDeck = data.decksList;
+    });
+  }
+
+  back() {
+    this.router.navigate(['']);
+  }
 }
